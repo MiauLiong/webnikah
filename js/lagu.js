bde454853066001775ab39db66b62cbe1c40e06a
miauliong <changmiauliong1409@gmail.com>
2022-11-27 21:37:23 +0700

    web nikah

---
+++ js/lagu.js
@@ -0,0 +1,11 @@
+// var musik = "lagu.mp3";
+
+// AOS.init({ once: true });
+
+// var audio = document.querySelector(".audio");
+// if (musik) {
+//   audio.src = musik;
+// }
+// function mulai() {
+//   audio.play();
+// }
