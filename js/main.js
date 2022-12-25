28e169d206612fd8fd1f7c1e08e87a341a71147e
miauliong <changmiauliong1409@gmail.com>
2022-12-25 21:33:03 +0700

    tambah fitur waktu

--- js/main.js
+++ js/main.js
@@ -18,3 +18,30 @@ $(window).on("load", function () {
 $(document).ready(function () {
   $(window);
 });
+
+// program hitung mundur
+const tanggalTujuan = new Date("Dec 25, 2022 22:00:00").getTime();
+
+const hitungMundur = setInterval(function () {
+  const sekarang = new Date().getTime();
+  const selisih = tanggalTujuan - sekarang;
+
+  const hari = Math.floor(selisih / (1000 * 60 * 60 * 24));
+  const jam = Math.floor((selisih % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const menit = Math.floor((selisih % (1000 * 60 * 60)) / (1000 * 60));
+  const detik = Math.floor((selisih % (1000 * 60)) / 1000);
+
+  const waktuacara1 = document.getElementById("waktuacara1");
+  waktuacara1.innerHTML = " Waktu acara akan dimulai, " + hari + " hari " + jam + " jam " + menit + " menit " + detik + " detik lagi ";
+
+  const waktuacara2 = document.getElementById("waktuacara2");
+  waktuacara2.innerHTML = " Waktu acara akan dimulai, " + hari + " hari " + jam + " jam " + menit + " menit " + detik + " detik lagi ";
+
+  const waktuacara3 = document.getElementById("waktuacara3");
+  waktuacara3.innerHTML = " Waktu acara akan dimulai, " + hari + " hari " + jam + " jam " + menit + " menit " + detik + " detik lagi ";
+
+  if (selisih < 0) {
+    clearInterval(hitungMundur);
+    TextTrackList.innerHTML = "Acara sedang di mulai";
+  }
+}, 1000);
